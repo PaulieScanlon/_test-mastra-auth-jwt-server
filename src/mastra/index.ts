a2c498dec5e4e7599e7e05856b3369d973eba142
Paul Scanlon <pauliescanlon@gmail.com>
2025-07-23 15:53:42 -0500

    chore: upgrade deps

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,31 +1,24 @@
 import { Mastra } from "@mastra/core/mastra";
 import { MastraJwtAuth } from "@mastra/auth";
-// import { PinoLogger } from "@mastra/loggers";
-// import { LibSQLStore } from "@mastra/libsql";
+import { PinoLogger } from "@mastra/loggers";
+import { LibSQLStore } from "@mastra/libsql";
 import { weatherWorkflow } from "./workflows/weather-workflow";
 import { weatherAgent } from "./agents/weather-agent";
 
 export const mastra = new Mastra({
   workflows: { weatherWorkflow },
   agents: { weatherAgent },
-  // storage: new LibSQLStore({
-  //   // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
-  //   url: ":memory:"
-  // }),
-  // logger: new PinoLogger({
-  //   name: "Mastra",
-  //   level: "info"
-  // }),
-  // Hard coding means Playground errors with a 403
+  storage: new LibSQLStore({
+    // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
+    url: ":memory:"
+  }),
+  logger: new PinoLogger({
+    name: "Mastra",
+    level: "info"
+  }),
   server: {
     experimental_auth: new MastraJwtAuth({
-      secret: "ABC123"
+      secret: process.env.MASTRA_JWT_SECRET
     })
   }
-  // Using an env var means the dev server errors because it can't find the env var
-  // server: {
-  //   experimental_auth: new MastraJwtAuth({
-  //     secret: process.env.MASTRA_JWT_SECRET
-  //   })
-  // }
 });
