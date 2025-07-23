394d910fbe6eb72dc590d89a8345ef9f987ee288
Paul Scanlon <pauliescanlon@gmail.com>
2025-07-23 16:07:25 -0500

    test: remove logger and storage

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,21 +1,21 @@
 import { Mastra } from "@mastra/core/mastra";
 import { MastraJwtAuth } from "@mastra/auth";
-import { PinoLogger } from "@mastra/loggers";
-import { LibSQLStore } from "@mastra/libsql";
-import { weatherWorkflow } from "./workflows/weather-workflow";
+// import { PinoLogger } from "@mastra/loggers";
+// import { LibSQLStore } from "@mastra/libsql";
+// import { weatherWorkflow } from "./workflows/weather-workflow";
 import { weatherAgent } from "./agents/weather-agent";
 
 export const mastra = new Mastra({
-  workflows: { weatherWorkflow },
+  // workflows: { weatherWorkflow },
   agents: { weatherAgent },
-  storage: new LibSQLStore({
-    // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
-    url: ":memory:"
-  }),
-  logger: new PinoLogger({
-    name: "Mastra",
-    level: "info"
-  }),
+  // storage: new LibSQLStore({
+  //   // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
+  //   url: ":memory:"
+  // }),
+  // logger: new PinoLogger({
+  //   name: "Mastra",
+  //   level: "info"
+  // }),
   server: {
     experimental_auth: new MastraJwtAuth({
       secret: process.env.MASTRA_JWT_SECRET
